bf2455f7019224ec47957164716b35837af7f81b
Tripognez Quentin <quentin_tripognez@ens.univ-artois.fr>
2025-06-26 15:53:10 +0200

    Mise à jour du composant App pour afficher le nom de la racine depuis les données de sectorisation et supprime les imports inutilisés

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
 import React from 'react';
+import sectorisation from './data/sectorisation.json';
 
 function App() {
-  return (
-      <div style={{ padding: "2rem" }}>
-        <h1>Permissions Tree</h1>
-      </div>
-  );
+    const root = sectorisation.data.roots[0]; // "France"
+
+    return (
+        <div style={{ padding: '2rem' }}>
+            <h1>{root.name}</h1>
+        </div>
+    );
 }
 
 export default App;
-
